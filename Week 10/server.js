9ccdf6245135979e779883ed0593d3338a420f0c
yueyiC <cai_cyy@126.com>
2021-03-07 23:37:01 +0800

    toybrowser

---
+++ Week 10/server.js
@@ -0,0 +1,47 @@
+const http = require("http");
+
+http.createServer((req,res)=>{
+  // console.log("req",req)
+  let body = []
+  req.on("error",(err)=>{
+    console.error(err)
+  }).on("data",(chunk)=>{
+    console.log("data",chunk)
+    body.push(chunk.toString());
+  }).on("end",()=>{
+    // console.log("body 1",body);
+    body=Buffer.concat(body).toString();
+    // console.log("body 2",body);
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end(
+        `<html maaa=a>
+        <head>
+            <style>
+            #container{
+                width:500px;
+                height:300px;
+                display:flex;
+                background-color:rgb(255,255,255);
+            }
+            #container #myid{
+                width:200px;
+                height:100px;
+                background-color:rgb(255,0,0);
+            }
+            #container .c1{
+                flex:1;
+                background-color:rgb(0,255,0);
+            }
+        </style>
+    </head>
+    <body>
+        <div id="container">
+            <div  id="myid"></div>
+            <div class="c1"></div>
+        </div>
+    </body>
+    </html>`)
+  })
+}).listen(8088)
+
+console.log("server started")
